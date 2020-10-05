bbf0e111adaed880b74598584f8f1630ad37a333
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-10-04 23:25:18 -0700

    add sign up link in navbar

--- zombie-packs-client/src/components/Navigation.js
+++ zombie-packs-client/src/components/Navigation.js
@@ -7,7 +7,8 @@ const Navigation = () => {
     <div>
       <Navbar  variant="dark" fixed="top" style={{ height: '6rem', background:'rgba(0,0,0, 0.60)'}}> 
       <Nav.Link><Link to={{ pathname: `/`}} style={{ color: '#FFF' }}><h2>Home</h2></Link></Nav.Link>
-      <Nav.Link><Link to={{ pathname: `/tactical_packages/`}} style={{ color: '#FFF' }}><h2>Zombie Tactical Packs</h2></Link></Nav.Link>   
+      <Nav.Link><Link to={{ pathname: `/tactical_packages/`}} style={{ color: '#FFF' }}><h2>Zombie Tactical Packs</h2></Link></Nav.Link> 
+      <Nav.Link><Link to={{ pathname: `/sign_up`}} style={{ color: '#FFF' }}><h2>Sign Up</h2></Link></Nav.Link>   
       </Navbar>
     </div>
   )
