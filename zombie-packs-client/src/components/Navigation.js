22b4bf5777c76d0e79dbfed396289dafbbc94dec
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-10-06 03:09:56 -0700

    add login link to navigation bar

--- zombie-packs-client/src/components/Navigation.js
+++ zombie-packs-client/src/components/Navigation.js
@@ -17,7 +17,8 @@ const Navigation = (props) => {
       <Navbar  variant="dark" fixed="top" style={{ height: '6rem', background:'rgba(0,0,0, 0.60)'}}> 
       <Nav.Link><Link to={{ pathname: `/`}} style={{ color: '#FFF' }}><h2>Home</h2></Link></Nav.Link>
       <Nav.Link><Link to={{ pathname: `/tactical_packages/`}} style={{ color: '#FFF' }}><h2>Zombie Tactical Packs</h2></Link></Nav.Link> 
-      <Nav.Link><Link to={{ pathname: `/sign_up`}} style={{ color: '#FFF' }}><h2>Sign Up</h2></Link></Nav.Link>   
+      <Nav.Link><Link to={{ pathname: `/sign_up`}} style={{ color: '#FFF' }}><h2>Sign Up</h2></Link></Nav.Link>
+      <Nav.Link><Link to={{ pathname: `/log_in`}} style={{ color: '#FFF' }}><h2>Log In</h2></Link></Nav.Link>    
       </Navbar>
     </div>
   )
