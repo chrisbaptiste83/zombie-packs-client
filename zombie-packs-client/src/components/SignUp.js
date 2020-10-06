def7f6d34373b358ef976ff790831f1eab2b2f50
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-10-06 03:11:26 -0700

    modify styling and update link to login page

--- zombie-packs-client/src/components/SignUp.js
+++ zombie-packs-client/src/components/SignUp.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { signupUser, ADD_TACTICALPACK } from "../actions/index";
-import { withRouter } from "react-router-dom";
+import { signupUser } from "../actions/index";
+import { withRouter, Link } from "react-router-dom";
 import { Form, Row, Col, Button, Jumbotron} from "react-bootstrap";
-import { Link } from "react-router-dom";
+
 
 class SignUp extends Component {
   constructor() {
@@ -41,7 +41,7 @@ class SignUp extends Component {
     return (
       <center>
           <Jumbotron style={{ background: 'rgba(0,0,0, 0.65)', width: '80rem', color:'white'}}>
-            <h3>Create Account!</h3><br></br>
+            <h3>Create An Account:</h3><br></br>
             <Form onSubmit={this.handleSubmit} className="signup">
               <Form.Group>
                 <Form.Control
@@ -52,7 +52,7 @@ class SignUp extends Component {
                   onChange={this.handleChange}
                 />
                 <div style={{ color: "red" }}>{this.props.usernameError}</div>
-              </Form.Group>
+              </Form.Group><br></br>
 
               <Form.Group>
                 <Form.Control
@@ -63,7 +63,7 @@ class SignUp extends Component {
                   onChange={this.handleChange}
                 />
                 <div style={{ color: "red" }}>{this.props.emailError}</div>
-              </Form.Group>
+              </Form.Group><br></br>
 
               <Row>
                 <Col>
@@ -99,7 +99,7 @@ class SignUp extends Component {
               </Button>
             </Form><br></br>
             <h4>Already have an account?</h4><br></br>
-              <Button to="/login">Login</Button>
+            <h4><Link to="/log_in">Log In</Link></h4>
       </Jumbotron> 
       </center>
     );
