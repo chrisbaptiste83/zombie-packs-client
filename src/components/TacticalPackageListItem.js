7d3dce54842d9b7d9dcbbfdcad48002c78adb06d
Chris Baptiste <chrisbaptiste83@gmail.com>
2021-01-27 04:31:19 -0700

    modify component to use updated json format

--- src/components/TacticalPackageListItem.js
+++ src/components/TacticalPackageListItem.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom'; 
 import {Card, Button} from 'react-bootstrap'
 
-const TacticalPackageListItem = ({id, attributes}) => { 
+const TacticalPackageListItem = (tacticalPackage) => { 
   return (
     <React.Fragment> 
         <Card style={{ background: 'rgba(0,0,0, 0.85)',color:'white', width: '65rem' }}>  
           <Card.Body>
-            <Card.Title><h2>{attributes.title}</h2></Card.Title> 
-            <Card.Text> <img src={attributes.image} width={500} height={240} alt="" /><br></br><br></br>
-              <h5>{attributes.description}</h5>
-              <small>created by: {attributes.creator}</small>
+            <Card.Title><h2>{tacticalPackage.title}</h2></Card.Title> 
+            <Card.Text> <img src={`http://localhost:3001/${tacticalPackage.avatar}`} width={500} height={240} alt="" /><br></br><br></br>
+              <h5>{tacticalPackage.description}</h5>
+              <small>created by: {tacticalPackage.creator}</small>
             </Card.Text> 
-            <Button style={{ background: 'black'}}><Link to={{ pathname: `/tactical_packages/${id}`}}> View Tac Pack</Link></Button>
+            <Button style={{ background: 'black'}}><Link to={{ pathname: `/tactical_packages/${tacticalPackage.id}`}}> View Tac Pack</Link></Button>
           </Card.Body>
         </Card><br></br>
     </React.Fragment>
